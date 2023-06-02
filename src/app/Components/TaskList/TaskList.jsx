f942d9687d4c299ffb7db9787e32a8b6bdde406e
Rahul kumar hotta <rahulkumar.hotta@corp.knowledgelens.com>
2023-06-02 18:52:01 +0530

    added no item in list message

--- src/app/Components/TaskList/TaskList.jsx
+++ src/app/Components/TaskList/TaskList.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import TaskCard from '../TaskCard/TaskCard'
-import './TaskList.css'
+import React from "react";
+import TaskCard from "../TaskCard/TaskCard";
+import "./TaskList.css";
 function TaskList(props) {
   return (
-    <div className='taskList__container'>
-        {props.taskList.map((item)=>{
+    <div className="taskList__container">
+      {props.taskList.length == 0 ? (
+        <h1>You dont have any tasks</h1>
+      ) : (
+        props.taskList.map((item) => {
+          return <TaskCard key={item.taskId} task={item} />;
+        })
+      )}
+      {/* {props.taskList.map((item)=>{
             return (
                 <TaskCard key={item.taskId} task={item}/>
             )
-        })}
+        })} */}
     </div>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
